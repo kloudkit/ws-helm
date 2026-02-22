f7b3d83e8ee68204157f7a8eaebad576a7a20d82
Dov Benyomin Sohacheski <b@kloud.email>
2026-02-22 13:05:35 +0200

    🔀 Add routing

--- shared/scripts/helm-lint-with-deps.sh
+++ shared/scripts/helm-lint-with-deps.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 chart="$1"
 
 helm repo add bjw-s https://bjw-s-labs.github.io/helm-charts --force-update
+helm repo add traefik https://traefik.github.io/charts --force-update
 helm dependency build "$chart"
 helm lint "$chart"
